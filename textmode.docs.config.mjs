6daa574a9c2a28e4dcd4a26812ac52fa7e4fb446
humanbydefinition <hello@textmode.art>
2026-07-31 00:09:06 +0200

    chore: update package dependencies and configuration for textmode.docs

---
+++ textmode.docs.config.mjs
@@ -0,0 +1,12 @@
+import { defineDocs } from '@textmode/docs';
+
+export default defineDocs({
+	memberPageKinds: ['method'],
+	checks: {
+		docstrings: ['function', 'method', 'accessor'],
+		examples: ['function', 'method', 'accessor'],
+	},
+	examples: {
+		checkSketches: false,
+	},
+});
